84d067f2ca253feaa3205453687c7ba4c30554cd
Karl Kroll <osu8371@oakley01.osc.edu>
2015-03-24 15:55:28 -0400

    fixed a typo

--- QC_Database/qc/application/views/login_view.php
+++ QC_Database/qc/application/views/login_view.php
@@ -11,7 +11,7 @@
       <input type="text" size="20" id="username" name="username"/>
       <br/>
       <label for="password">Password:</label>
-      <input type="password" size="20" id="passowrd" name="password"/>
+      <input type="password" size="20" id="password" name="password"/>
       <br/>
       <input type="submit" value="Login"/>
     </form>
