be5c219bfd4a5caa70d65b3710600dbe2ffc1be3
Karl Kroll <karl.kroll@osumc.edu>
2015-04-03 15:29:46 -0400

    added empty configuration files

---
+++ QC_Database/read/constant/config.py
@@ -0,0 +1,39 @@
+#This file contains all the information needed to connect to the MySQL database
+
+
+DB_HOST=""
+DB_USER=""
+DB_PASS=""
+DB_PORT=""
+DB_NAME=""
+
+"""
+This constant should contain where you copy the files
+for the front enf of the project.
+
+You should have a server (local) running on the machine that you are running the code.
+
+**Full path
+
+"""
+WEB_APP_PATH=""
+
+
+
+
+#DEBUGGING OPTIONS DB
+#OPTIONS:
+#
+#	-NONE
+#	-ALL
+#	-INSERTS
+#	-UPDATES
+#	-DELETES
+#	-SELECTS
+#	-CREATES
+#	-VIEWS
+#	or any combination of these in an array
+#	["INSERTS", "SELECTS"]
+
+DB_DEBUG_MODE = "NONE"
+DEBUG_MODE = False
